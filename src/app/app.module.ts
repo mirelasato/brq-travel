ed12211fc1eb7c97c48be6363217e8e138421dc1
Kamila Bezerra da Silva <kamilabezerra@brq.com>
2019-12-03 16:03:00 -0300

    desenvolvimento do component topo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TopoComponent } from './topo/topo.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    TopoComponent,
     
   ],
   imports: [
