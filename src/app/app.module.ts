af22547bcd89c2dceadd8df4f8661a286561b3e8
Kamila Bezerra da Silva <kamilabezerra@brq.com>
2019-12-06 11:39:14 -0300

    correção do conflito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TopoComponent } from './topo/topo.component';
-<<<<<<< HEAD
 import { RodapeComponent } from './rodape/rodape.component';
-=======
 import { FormComponent } from './form/form.component';
->>>>>>> 7290147cdc678b1dcb7768dbb158db2df53e3359
 
 
 @NgModule({
@@ -17,12 +14,8 @@ import { FormComponent } from './form/form.component';
     AppComponent,
     HomeComponent,
     TopoComponent,
-<<<<<<< HEAD
     RodapeComponent,
-    
-=======
     FormComponent,
->>>>>>> 7290147cdc678b1dcb7768dbb158db2df53e3359
   ],
   imports: [
     BrowserModule,
