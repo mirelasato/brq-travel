04350a745997992f0d9bad148adfb6e9a0efa70b
Vinicius Rossi <vini.rossisilva@gmail.com>
2019-12-16 15:13:58 -0300

    Removido o Component Card Choice e inserido no Card-Travel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,12 @@ import { HomeComponent} from './home/home.component';
 import { UserSignInComponent} from './user-forms/user-sign-in/user-sign-in.component';
 import { UserSignUpComponent } from './user-forms/user-sign-up/user-sign-up.component';
 import { CardTravelComponent } from './card-travel/card-travel.component';
-import { CardchoicetravelComponent } from './cardchoicetravel/cardchoicetravel.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
   { path: 'home',  component: HomeComponent},
   { path: 'entrar', component: UserSignInComponent},
   { path: 'cadastrar', component: UserSignUpComponent},
-  { path: 'pacotes', component: CardchoicetravelComponent},
   { path: 'pacotes', component: CardTravelComponent},
   { path: 'visualizacao', component: HomeComponent },
 
