28a6651a8e33aa0666d365bfcaefd2524bd8ea0a
Vinicius Rossi <vini.rossisilva@gmail.com>
2019-12-16 10:23:55 -0300

    Removido linha com Bootstrapp a mais e organizado o App Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,14 +11,18 @@ import { TopoComponent } from './topo/topo.component';
 import { UserSignUpComponent } from './user-forms/user-sign-up/user-sign-up.component';
 import { UserSignInComponent } from './user-forms/user-sign-in/user-sign-in.component';
 import { RodapeComponent } from './rodape/rodape.component';
-import { CollapseModule, BsDropdownModule } from 'ngx-bootstrap';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
 import { CardTravelComponent } from './card-travel/card-travel.component';
 import { CardchoicetravelComponent } from './cardchoicetravel/cardchoicetravel.component';
-import { NgxMaskModule } from 'ngx-mask';
+
+import { CollapseModule, BsDropdownModule } from 'ngx-bootstrap';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { NgxMaskModule } from 'ngx-mask';
+
+
+
+
+
 
 
 @NgModule({
