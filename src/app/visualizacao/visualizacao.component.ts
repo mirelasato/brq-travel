2f120a0467f340c11a8bbfdc6386393e9ec0f387
Kamila Bezerra da Silva <kamilabezerra@brq.com>
2019-12-19 16:22:44 -0300

    implementando uma classe com objeto mockado

--- src/app/visualizacao/visualizacao.component.ts
+++ src/app/visualizacao/visualizacao.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { Pacote} from '../shared/models/pacotes.model';
-import { Pacote } from '../shared/models/pacotes.model';
+import { Detalhes } from '../shared/models/detalhes.model';
 
 
 @Component({
@@ -9,72 +8,50 @@ import { Pacote } from '../shared/models/pacotes.model';
   styleUrls: ['./visualizacao.component.css']
 })
 export class VisualizacaoComponent implements OnInit {
- 
-  titulo: string;
-  valor: string;
-  url: string;
 
-  pacotes: Pacote[];
+  pacotes: Detalhes[];
+  imagens: any[];
 
   constructor() { }
 
   ngOnInit() {
-    this.getPacotes();
+    this.getDetalhes  ();
 
 
-    this.titulo = 'Caldas Novas';
-    this.valor = '150,00';
-    this.url = '/assets/img/capa-destino.jpg';
-
-console.log('this.pacotes' , this.pacotes);
+console.log('this.pacotes' , this.pacotes[0]);
 
 
   }
-  getPacotes() {
+  getDetalhes() {
 
     this.pacotes = [
       {
-      id: 1,
-      titulo: 'Caldas Novas',
-      anunciante: '',
-      valor: 300,
-      destaque: false,
-      descricao: '',
-      categoria: '',
-      imagens: [
-          {
-            url:'assets/img/capa-destinos.jpg',
-          },
-      ]
-    },
-    {
-      id: 2,
-      titulo: 'Ilha Bela',
-      anunciante: '',
-      valor: 300,
-      destaque: false,
-      descricao: '',
-      categoria: '',
-      imagens: [
-        {
-          id: 1,
-          name: 'rodrigo2',
-          url: 'assets/img/capa-destinos.jpg',
-        },
-        {
-          id: 2,
-          name: 'kamila2',
-          url: 'assets/img/capa-destinos.jpg'
-        },
-        {
-          id: 3,
-          name: 'vinicios2',
-          url: 'assets/img/capa-destinos.jpg'
-        },
-      ]
-    },
-  ];
+        id: 1,
+        titulo: 'Caldas Novas',
+        anunciante: '',
+        valor: 300,
+        destaque: false,
+        data: '',
+        feriado: '',
+        descricao: '',
+        tipo: '',
+        vagas: 30,
+        imagens: [
+            {
+              url: '../assets/img/capa-destinos.jpg',
+            },
+            {
+              url: '../assets/img/capa-destinos.jpg',
+            },
+            {
+              url: '../assets/img/capa-destinos.jpg',
+            },
+        ]
+      },
+    ];
 
-  }
+  this.imagens = this.pacotes[0].imagens;
 
+  
+}
 }
