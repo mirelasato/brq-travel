5939821272e14c594fdf27a4d6b31660ec5e4c1d
Kamila Bezerra da Silva <kamilabezerra@brq.com>
2019-12-19 10:50:36 -0300

    desenvolvimento do componente visualizacao

--- src/app/shared/models/pacotes.model.ts
+++ src/app/shared/models/pacotes.model.ts
@@ -1,4 +1,4 @@
-export class View {
+export class Pacote {
     public id: number;
     public categoria: string;
     public titulo: string;
@@ -6,6 +6,5 @@ export class View {
     public anunciante: string;
     public valor: number;
     public destaque: boolean;
-    public imagens: Array<object>;
-
+    public imagens: Array<T>;
 }
--- src/app/visualizacao/visualizacao.component.ts
+++ src/app/visualizacao/visualizacao.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { View} from '../shared/models/view.model';
+// import { Pacote} from '../shared/models/pacotes.model';
+import { Pacote } from '../shared/models/pacotes.model';
+
 
 @Component({
   selector: 'app-visualizacao',
@@ -7,16 +9,71 @@ import { View} from '../shared/models/view.model';
   styleUrls: ['./visualizacao.component.css']
 })
 export class VisualizacaoComponent implements OnInit {
- view = View;
+ 
   titulo: string;
   valor: string;
+  url: string;
+
+  pacotes: Pacote[];
 
   constructor() { }
 
   ngOnInit() {
+    this.getPacotes();
+
+
     this.titulo = 'Caldas Novas';
     this.valor = '150,00';
+    this.url = '/assets/img/capa-destino.jpg';
 
+console.log('this.pacotes' , this.pacotes);
+
+
+  }
+  getPacotes() {
+
+    this.pacotes = [
+      {
+      id: 1,
+      titulo: 'Caldas Novas',
+      anunciante: '',
+      valor: 300,
+      destaque: false,
+      descricao: '',
+      categoria: '',
+      imagens: [
+          {
+            url:'assets/img/capa-destinos.jpg',
+          },
+      ]
+    },
+    {
+      id: 2,
+      titulo: 'Ilha Bela',
+      anunciante: '',
+      valor: 300,
+      destaque: false,
+      descricao: '',
+      categoria: '',
+      imagens: [
+        {
+          id: 1,
+          name: 'rodrigo2',
+          url: 'assets/img/capa-destinos.jpg',
+        },
+        {
+          id: 2,
+          name: 'kamila2',
+          url: 'assets/img/capa-destinos.jpg'
+        },
+        {
+          id: 3,
+          name: 'vinicios2',
+          url: 'assets/img/capa-destinos.jpg'
+        },
+      ]
+    },
+  ];
 
   }
 
