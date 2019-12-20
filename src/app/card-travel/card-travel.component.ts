545b34ea73622a1aeb4a14079cdbd056bf1a5a12
Kamila Bezerra da Silva <kamilabezerra@brq.com>
2019-12-20 09:44:50 -0300

    databind do component card

--- src/app/card-travel/card-travel.component.ts
+++ src/app/card-travel/card-travel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from '../shared/models/card.model';
+
 
 @Component({
   selector: 'app-card-travel',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-travel.component.css']
 })
 export class CardTravelComponent implements OnInit {
+ card: Card[];
+ imagens: any[];
 
   constructor() { }
 
   ngOnInit() {
+    
+
+    this.card = [
+      {
+          titulo: 'Caldas Novas',
+          descricao: 'Uma breve descrição da viagem',
+          data: '12/04/2020',
+          valor: 300,
+          imagens: [{
+           url: 'assets/img/capa-destino',
+          }
+        ]
+      }
+    ]
+
   }
 
 }
---
+++ src/app/shared/models/card.model.ts
@@ -0,0 +1,7 @@
+export class Card {
+    public titulo: string;
+    public descricao: string;
+    public data: string;
+    public valor: number;
+    public imagens: Array<object>;
+}
