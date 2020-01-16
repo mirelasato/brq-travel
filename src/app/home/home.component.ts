7ea94cf4f1fb05dfc428e46668d2527b2ef52225
Vinicius Rossi <vini.rossisilva@gmail.com>
2020-01-16 18:16:24 -0300

    Criação no JSON DB, service e integração do component com a service.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { PacotesDestinoService } from '../shared/services/pacotes-destino.service';
 import { Destino } from '../shared/models/destino';
+import { HomeBannerService } from '../shared/services/home-banner.service';
+import { Banner } from '../shared/models/banner';
 
 
 @Component({
@@ -12,13 +14,32 @@ export class HomeComponent implements OnInit {
 
   destaqueCards: Destino[];
   destaqueCardsDisplay: Destino[];
-  
-  constructor(private service: PacotesDestinoService) { }
+
+  feriadoImg: Banner[];
+  feriadoImgDisplay: Banner[];
+
+  final: any;
+
+  constructor(
+    private service: PacotesDestinoService,
+    private serviceBanner: HomeBannerService
+    ) { }
 
   ngOnInit() {
     this.service.lista()
     .subscribe(dados => {this.destaqueCards = dados;
-      this.destaqueCardsDisplay = this.destaqueCards.filter(x => x.destaque === true
-     )});
+                         this.destaqueCardsDisplay = this.destaqueCards.filter(x => x.destaque === true)});
+
+
+    this.serviceBanner.listaBanner()
+    .subscribe(img => {this.feriadoImg = img;
+                      //  this.feriadoImgDisplay = this.feriadoImg.filter(y => y.id === 1);
+
+                      //  this.final = this.feriadoImgDisplay[0];
+                       console.log('teste', this.feriadoImg);
+                       this.final = this.feriadoImg[0];
+                       console.log('AAAAA', this.final);
+    // tslint:disable-next-line: semicolon
+    })
   }
 }
---
+++ src/app/shared/services/home-banner.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { Banner } from '../models/banner';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeBannerService {
+
+  private readonly API = 'http://localhost:3000/feriado';
+
+  constructor(private http: HttpClient) { }
+
+  listaBanner() {
+    return this.http.get<Banner[]>(this.API)
+    .pipe(
+      tap(console.log)
+    )
+  }
+}
