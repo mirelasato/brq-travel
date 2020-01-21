e1632b3a47bde50d78082c60040d3a9eaf999b6d
Kamila Bezerra da Silva <kamilabezerra@brq.com>
2020-01-21 16:49:57 -0300

    desenvolvimento e integração das rotas com a api.json

---
+++ src/app/shared/services/visualizacao.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http'; //importar desse jeito, lembrar de quando for se referir a esse importe ser httpclient
+import { Injectable} from '@angular/core';
+
+import{ URL_API} from './app.api';
+import { Detalhes } from '../models/detalhes.model';
+
+
+@Injectable()
+export class VisualizacaoService{
+    private url_api = 'http://localhost:3000/destinos' //ponto unico para modificação
+    
+    constructor(private http: HttpClient){}
+    
+    
+public getOfertas(): Promise<Detalhes[]> 
+{
+    //efetuar uma requisição http
+    return this.http.get(`${URL_API}?destaque=true`)//observable
+    .toPromise()  //para converter esse observable para uma promise 
+    .then((resposta: any) => resposta)
+    //retornar uma promise Oferta[]
+    }
+    public getDetalhesPorCategoria(feriado: string) : Promise<Detalhes[]> {
+        return this.http.get(`${URL_API}?feriado=${feriado}`)
+            .toPromise()
+            .then((resposta: any)=>  resposta)//objeto literal
+    }
+    public getDetalhesPorId(id: number): Promise<Detalhes>{//vai retornar uma promise
+    return this.http.get(`${URL_API}?id=${id}`)// filtra através dos atributos de cada objeto
+    .toPromise()//converte uma observable para uma promise
+    .then((resposta: any)=> { // quando estiver resolvido recebe uma resposta com base na ação
+        //o metodo .ishift ele extrai a primeira posição de um array 
+        return resposta[0]
+    })
+    }
+    getDestino(id: string) {
+        return this.http.get<Detalhes>(this.url_api + '/' + id);
+      }
+}
--- src/app/visualizacao/visualizacao.component.ts
+++ src/app/visualizacao/visualizacao.component.ts
@@ -4,34 +4,47 @@ import { Destino } from '../shared/models/destino';
 import { ActivatedRoute } from '@angular/router';
 import { ShoppingCartService } from '../shopping-cart.service';
 import { Detalhes } from '../shared/models/detalhes.model';
+import { VisualizacaoService } from '../shared/services/visualizacao.service';
+import { TabDirective } from 'ngx-bootstrap';
 
 
 @Component({
   selector: 'app-visualizacao',
   templateUrl: './visualizacao.component.html',
   styleUrls: ['./visualizacao.component.css'],
-  
+  providers: [VisualizacaoService]
 })
 export class VisualizacaoComponent implements OnInit {
 
-  pacotes: Detalhes[];
+  public oferta: Detalhes;
+
   imagens: any[];
   destaqueCards: Destino[];
   destaqueCardsDisplay: Destino[];
   // public pacotes: Detalhes;
-  constructor(private service: ShoppingCartService) { }
+  // VisualizacaoService: any;
+  value: string;
+  onSelect(data: TabDirective): void {
+    this.value = data.heading;
+  }
+  constructor(private service: ShoppingCartService,
+    private route: ActivatedRoute,
+    private visualizacaoService: VisualizacaoService) { }
 
   ngOnInit() {
-    this.service.addToCart()
-    .subscribe(dados => {this.pacotes = dados;
-      // tslint:disable-next-line:align
-      this.pacotes = this.pacotes.filter(x => x.destaque === true
-    )});
+    // this.service.addToCart()
+    // .subscribe(dados => {this.pacotes = dados;
+    //   // tslint:disable-next-line:align
+    //   this.pacotes = this.pacotes.filter(x => x.destaque === true
+    // )});
+console.log(this.route.snapshot.params['id']);
+     this.visualizacaoService.getDetalhesPorId(this.route.snapshot.params['id'])
+     .then(( oferta: Detalhes) => {
+       this.oferta = oferta;
 
-    this.getDetalhes  ();
-  }
-  getDetalhes() {
-   return this.pacotes
+ }
+     )
+   }
 
-  }
 }
+
