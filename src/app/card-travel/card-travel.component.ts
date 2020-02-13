dce9b2875038fc532e265bc5304cf24dfc40a7b4
Mirela Harumi Sato <mirelasato@brq.com>
2020-02-13 19:02:00 -0300

    redesign dos cards, demais validações do formulário de pagamento

--- src/app/card-travel/card-travel.component.ts
+++ src/app/card-travel/card-travel.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
@@ -9,11 +8,11 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CardTravelComponent implements OnInit {
 
   @Input() destino: any;
-  
+
   constructor() { }
 
   ngOnInit() {
-    
+
   }
-  
+
 }
