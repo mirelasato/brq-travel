20b1fe4ed37279be4c33cfedfa6acce496a985d1
Mirela Harumi Sato <mirelasato@brq.com>
2020-02-19 19:22:19 -0300

    Corrigido o armazenamento do localStorage com os itens do carrinho

--- src/app/shared/services/shopping-cart.service.ts
+++ src/app/shared/services/shopping-cart.service.ts
@@ -22,27 +22,32 @@ export class ShoppingCartService {
   private items: Item[] = [];
   public total = new BehaviorSubject<number>(0);
 
+  private currentCartCount = new BehaviorSubject(0);
+  currentMessage = this.currentCartCount.asObservable();
+  private itemsCart: Product[] = [];
+  public pacotes: Detalhes[];
+  id: any;
 
-  constructor( ) { 
-    this.product =  this.product = [
-      { id: 'oferta1', name: 'Arraial do Cabo',  price: 500,  image: "assets/travel-packages/arraial-do-cabo/image1.jpg" },
-      { id: 'oferta2', name: 'Campos do Jordão', price: 160,  image: "assets/travel-packages/campos-do-jordao/image1.jpg" },
-      { id: 'oferta3', name: 'Foz do Iguaçu',    price: 690,  image: "assets/travel-packages/foz/image1.jpg" },
-      { id: 'oferta4', name: 'São Roque',        price: 200,  image: "assets/travel-packages/sao-roque/image1.jpg" },
-      { id: 'oferta5', name: 'Capitólio',        price: 200,  image: "assets/travel-packages/capitolio/image1.jpg" },
-      { id: 'oferta6', name: 'Ilhabela',         price: 120,  image: "assets/travel-packages/ilhabela/image1.jpg" },
-      { id: 'oferta7', name: 'Monte Verde',      price: 120,  image: "assets/travel-packages/monte-verde/image1.jpg" },
-      { id: 'oferta8', name: 'TESTE 8',          price: 120,  image: "assets/travel-packages/monte-verde/image3.jpg" },
-      { id: 'oferta9', name: 'TESTE 9',          price: 200,  image: "assets/travel-packages/monte-verde/image3.jpg" },
+  constructor() {
+    this.product = this.product = [
+      { id: 'oferta1', name: 'Arraial do Cabo', price: 500, image: "assets/travel-packages/arraial-do-cabo/image1.jpg" },
+      { id: 'oferta2', name: 'Campos do Jordão', price: 160, image: "assets/travel-packages/campos-do-jordao/image1.jpg" },
+      { id: 'oferta3', name: 'Foz do Iguaçu', price: 690, image: "assets/travel-packages/foz/image1.jpg" },
+      { id: 'oferta4', name: 'São Roque', price: 200, image: "assets/travel-packages/sao-roque/image1.jpg" },
+      { id: 'oferta5', name: 'Capitólio', price: 200, image: "assets/travel-packages/capitolio/image1.jpg" },
+      { id: 'oferta6', name: 'Ilhabela', price: 120, image: "assets/travel-packages/ilhabela/image1.jpg" },
+      { id: 'oferta7', name: 'Monte Verde', price: 120, image: "assets/travel-packages/monte-verde/image1.jpg" },
+      { id: 'oferta8', name: 'TESTE 8', price: 120, image: "assets/travel-packages/monte-verde/image3.jpg" },
+      { id: 'oferta9', name: 'TESTE 9', price: 200, image: "assets/travel-packages/monte-verde/image3.jpg" },
 
     ];
   }
 
-  getTotal$(){
+  getTotal$() {
     return this.total.asObservable();
   }
 
-  calcTotal(){
+  calcTotal() {
     let sum = 0;
     this.items.forEach(item => {
       sum += item.quantity;
@@ -50,12 +55,12 @@ export class ShoppingCartService {
     return sum;
   }
 
-  addItem(item: any){
+  addItem(item: any) {
     this.items.push(item);
     this.total.next(this.calcTotal());
     //save to localStore
   }
-  
+
   findAll(): Product[] {
     return this.product;
   }
@@ -74,66 +79,67 @@ export class ShoppingCartService {
   }
 
 
-  private currentCartCount = new BehaviorSubject(0);
-  currentMessage = this.currentCartCount.asObservable();
-  public itemsCart: Product[] = [];
-  public pacotes: Detalhes[];
-  id: any;
+
 
   updateCartCount(count: number) {
     this.currentCartCount.next(count);
   }
 
   addProductToCart(product: Product) {
+    const cart = JSON.parse(localStorage.getItem('cart'));
+    cart.forEach(element => {
+      this.itemsCart.push(element);
+    });
     this.itemsCart.push(product);
-    console.log('pacote', this.itemsCart);
-    localStorage.setItem('product', JSON.stringify(this.itemsCart));
+    console.log(this.itemsCart);
+    localStorage.removeItem('cart');
+    localStorage.setItem('cart', JSON.stringify(this.itemsCart));
   }
 
 
   getProductFromCart() {
-    if (localStorage.getItem('product') === null ) {
+    if (localStorage.getItem('product') === null) {
       this.product = [];
     } else {
       this.product = JSON.parse(localStorage.getItem('product'));
     }
     return localStorage.getItem('product')
-    
+
   }
 
   removeItem() {
     return localStorage.removeItem('product');
   }
 
-//   removeItem(Product){
-//     this.product.splice(this.product.indexOf(Product), 1)
-//     //salva na sessão
-//     sessionStorage.setItem("cart",JSON.stringify(this.product));
-// }
+  //   removeItem(Product){
+  //     this.product.splice(this.product.indexOf(Product), 1)
+  //     //salva na sessão
+  //     sessionStorage.setItem("cart",JSON.stringify(this.product));
+  // }
 
-// recalculateCart() {
- 
+  // recalculateCart() {
 
-//   $ ('.product').each(function () {
-//     this.total + = parseFloat ($ (this).children('.quantity').text());
-//   });
-// }
+
+  //   $ ('.product').each(function () {
+  //     this.total + = parseFloat ($ (this).children('.quantity').text());
+  //   });
+  // }
 
 
 
 
   totalIns(): number {
     return this.product
-    .map(item => item.price)
-    .reduce(( prev, value) => prev + value, 0);
+      .map(item => item.price)
+      .reduce((prev, value) => prev + value, 0);
   }
 
   installment(): number {
     return Math.max.apply(
       Math, this.product
-      .map(function(prod){
-        return prod.price;
-      })
+        .map(function (prod) {
+          return prod.price;
+        })
     )
 
   }
